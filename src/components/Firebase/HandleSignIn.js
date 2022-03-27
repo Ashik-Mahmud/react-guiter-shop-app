5314980ec84a5b4c95deb17e377eee22c2c11508
ashikmahmud <ashikmamud187@gmail.com>
2022-03-27 23:09:00 +0600

    added firebase authentication successfully

--- src/components/Firebase/HandleSignIn.js
+++ src/components/Firebase/HandleSignIn.js
@@ -2,21 +2,34 @@ import { Storage } from "../Storage/Storage";
 const handleSignInWithFirebase = (response , setIsLoggedIn) =>{
     
     if(response){
-        setIsLoggedIn(true)
+        
        const {email, displayName, photoURL} = response.user;
        const items = Storage();
        const id = Math.round(Math.random() * 10000);
-      items.push({
-        id,
-        name: displayName,
-        email,
-        username: email,
-        password: null,
-        avatar: photoURL,
-      });
-       
-        localStorage.setItem('user-info', JSON.stringify(items))
+
+      const isHas = items.find(item => item.email === email);
+      if(isHas){
         sessionStorage.setItem("user", email)
+        setIsLoggedIn(true)
+      }else{
+        setIsLoggedIn(true)
+        items.push({
+            id,
+            name: displayName,
+            email,
+            username: email,
+            password: null,
+            avatar: photoURL,
+          });
+           
+            localStorage.setItem('user-info', JSON.stringify(items))
+            sessionStorage.setItem("user", email)
+            sessionStorage.setItem("user", email)
+       
+      }
+
+
+     
         
     }
 }
